0f9cf9e40c0199ec350db739ecfc04cb591afa87
Alan-Miller <alan.ashman@gmail.com>
2017-11-06 21:35:28 -0700

    working microservices files with simple add functionality

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require('express')
     , bodyParser = require('body-parser')
-    , seneca = require('seneca')
+    , seneca = require('seneca')()
     , app = express();
 
 app.use(bodyParser.json());
+
+seneca.use('./process.js');
+
+seneca.act({ role: 'process', cmd: 'sum', numbers: [1, 2, 3, 4] }, function (err, result) {
+    console.log('result', result);
+});
 
 app.listen(3000, _ => { console.log(`Listening on 3000.`); });
