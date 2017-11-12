8d2f3cec9b8ec8c09d688e7f184a8fb576e2fc9d
Alan-Miller <alan.ashman@gmail.com>
2017-11-12 13:30:22 -0700

    edits

--- server/process.js
+++ server/process.js
@@ -1,13 +1,11 @@
-module.exports = function (options) {
+// module.exports = function (options) {
   const seneca = this;
 
   seneca.add({ role: 'process', cmd: 'sum' }, sum);
 
   function sum(args, done) {
     var numbers = args.numbers;
-    var result = numbers.reduce(function (a, b) {
-      return a + b;
-    }, 0);
-    done(null, { result: result });
+    var result = numbers.reduce((a, b) => a + b, 0);
+    done(null, { result });
   }
-}
+// }
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-const express = require('express')
-    , bodyParser = require('body-parser')
-    , seneca = require('seneca')()
-    , app = express();
+// const express = require('express')
+    // , bodyParser = require('body-parser')
+    // , app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+const seneca = require('seneca')();
 
 seneca.use('./process.js');
 
-seneca.act({ role: 'process', cmd: 'sum', numbers: [1, 2, 3, 4] }, function (err, result) {
+seneca.act({ role: 'process', cmd: 'sum', numbers: [1, 2, 3, 4] }, (err, result) => {
     console.log('result', result);
 });
 
-app.listen(3000, _ => { console.log(`Listening on 3000.`); });
+// app.listen(3000, () => { console.log(`Listening on 3000.`); });
